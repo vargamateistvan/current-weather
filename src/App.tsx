c25fbf70bd591daba220cb91ee874b7dfa8d8fb0
Varga Máté István <mateistvanvarga@gmail.com>
2019-07-30 16:14:33 +0200

    fix(App): Remove dotenv import.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './App.css';
 import WeatherCard from "./components/WeatherCard"
 import { getCurrentWeather } from "./utils/getWeather"
 
-require('dotenv').config()
-
 const App: React.FC = () => {
   const currentWeather = getCurrentWeather("Budapest")
 
