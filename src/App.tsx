a3762199e2ac9b3123fdecee2e474135f43cd11d
Varga Máté István <mateistvanvarga@gmail.com>
2019-07-30 16:07:47 +0200

    fix(dotenv): Dotenv fix.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import './App.css';
 import WeatherCard from "./components/WeatherCard"
 import { getCurrentWeather } from "./utils/getWeather"
 
+require('dotenv').config()
+
 const App: React.FC = () => {
   const currentWeather = getCurrentWeather("Budapest")
+
+  console.log("ENV", process.env)
 
   return (
     <div className="App">
--- src/utils/getWeather.ts
+++ src/utils/getWeather.ts
@@ -3,7 +3,7 @@
 const weatherAPI = "https://api.openweathermap.org/data/2.5"
 
 export async function getCurrentWeather(cityName: string) {
-    const response = await fetch(`${weatherAPI}/weather?q=${cityName}&APPID=${process.env.weatherAPIKey}`, {
+    const response = await fetch(`${weatherAPI}/weather?q=${cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`, {
         method: "GET",
         headers:{
             'Content-Type': 'application/json'
