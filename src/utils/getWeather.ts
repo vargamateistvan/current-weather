237245884b679adb73d8c7477f41b16ccabad47a
Varga Máté István <mateistvanvarga@gmail.com>
2019-08-02 13:24:54 +0200

    fix(No result): No result error fix.

--- src/utils/getWeather.ts
+++ src/utils/getWeather.ts
@@ -3,23 +3,43 @@
 const weatherAPI = "https://api.openweathermap.org/data/2.5"
 
 export async function getCurrentWeather(cityName: string) {
-    const response = await fetch(`${weatherAPI}/weather?q=${cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`, {
-        method: "GET",
-        headers:{
-            'Accept': 'application/json'
-        },
-    })
+    try {
+        const response = await fetch(`${weatherAPI}/weather?q=${cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`, {
+            method: "GET",
+            headers:{
+                'Accept': 'application/json'
+            },
+        })
+    
+        const result = await response.json()
 
-    return await response.json()
+        if(result.cod === "404") {
+            throw new Error("No city found!")
+        }
+        
+        return result
+    } catch(error) {
+        console.error(error)
+    }
 }
 
 export async function getHourlyForecast(cityName: string) {
-    const response = await fetch(`${weatherAPI}/forecast?q=${cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`, {
-        method: "GET",
-        headers:{
-            'Accept': 'application/json'
-        },
-    })
+    try {
+        const response = await fetch(`${weatherAPI}/forecast?q=${cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`, {
+            method: "GET",
+            headers:{
+                'Accept': 'application/json'
+            },
+        })
+    
+        const result = await response.json()
 
-    return await response.json()
+        if(result.cod === "404") {
+            throw new Error("No city found!")
+        }
+
+        return result
+    } catch(error) {
+        console.error(error)
+    }
 }
